61b15b999160b03247a3c36543a1667b694be84c
Florence <florence.lo@hotmail.com>
2018-02-05 12:15:42 +1300

    update

---
+++ js/D3Scatter.js
@@ -0,0 +1,71 @@
+
+var data;
+d3.json("js/mockData.json", function(error, jsonData){
+    if(error){
+        console.log("something went wrong");
+        return;
+    }
+
+    data = jsonData;
+
+    var height = 400;
+        width = 600;
+
+    var yMax = 0;
+    var xMax = 0;
+
+    for (let i = 0; i < data.length; i++) {
+        if (data[i].age > yMax){
+            yMax = data[i].age
+        }    
+        if (data[i].weight > xMax) {
+            xMax = data[i].weight
+        }
+    }        
+
+    var yScale = d3.scaleLinear()
+        .domain([0, yMax])
+        .range([0, height])
+
+    var xScale = d3.scaleLinear()
+        .domain([0, xMax])
+        .range([0, width])
+
+    var color = d3.scaleLinear()
+        // dividing xMax allows more colour range
+        .domain([0, xMax/2, xMax])
+        .range(['#00a2ff', '#0e7aa5', '#d467ff'])
+    
+    d3.select('body').append('svg')
+        .attr('width', width)
+        .attr('height', height)
+        .style('background-color', 'lightBlue')
+        .selectAll('circle')
+            .data(data)
+            .enter().append('circle')
+                .classed('circle', true)
+                .style('fill', function(d, i){
+                    return color(d.weight)
+                })
+                .attr('r', '8')
+                .attr('cx', function (d) { //d for data
+                    return xScale(d.weight)
+                })
+                .attr('cy', function (d) {
+                    return height - yScale(d.age)
+                })
+                .on('mouseover', function(d){
+                    // to select 'this' object
+                    d3.select(this)
+                        .style('opacity', 0.5)
+                        .append('div')
+                            .style('background-color', 'white')
+                })
+                .on('mouseout', function (d) {
+                    // to select 'this' object
+                    d3.select(this)
+                        .style('opacity', 1)
+                })
+
+})
+
--- js/barChart.js
+++ js/barChart.js
@@ -1,8 +1,21 @@
-var barData = [12, 49, 39, 20, 10, 4, 23, 54, 23, 100, 56, 90, 12, 70, 32];
+// var barData = [0, 20, 40, 60, 80, 100, 120, 100, 80, 60, 40, 20, 10, 30, 50, 70, 90];
+var chartData = [0, 40, 20, 50, 24, 32, 32, 54, 87, 34, 94, 120, 37, 73, 12, 9, 40];
 var height = 400;
     width = 600;
     barWidth = 50;
     barOffSet = 5;
+
+var yScale = d3.scaleLinear()
+    // Which values represent the range. d3.max is finding the maximum value in barData array
+    .domain([0, d3.max(barData)])
+    // what is the range/dimensions of the container
+    .range([0, height])
+
+    // ScaleBand, re ajusting width of graph
+var xScale = d3.scaleBand()
+    // d3.range will give two values and will give increments between the two. It will take barData length.
+    .domain(d3.range(0, barData.length))
+    .range([0, width])
 
 
 d3.select('body').append('svg')
@@ -12,16 +25,16 @@ d3.select('body').append('svg')
     .selectAll('rect') //rect is svg term for rectangle
         .data(barData)
         .enter().append('rect')
-
             .style('fill', '#c0392b')
-            .attr('width', barWidth)
+            .attr('width', xScale.bandwidth())
             .attr('height', function(d){ //d for data
-                return d
+                return yScale(d)
             })
             .attr('x', function(d, i) { //i for iteration
-                return i * (barWidth+barOffSet)
+                return xScale(i)
             })
             .attr('y', function(d) {
-                return height - d
+                return height - yScale(d)
             }).exit()
-            // must put exit() at the end to stop the 'loop'
+            // must put exit() at the end to stop the 'loop' of data
+
